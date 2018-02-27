487b2ac6178efba002923656f54623452393904a
ctch3ng <32386442+ctch3ng@users.noreply.github.com>
2018-02-27 22:27:48 +0800

    Update PyCS_XQuartz.sh

--- PyCS_XQuartz.sh
+++ PyCS_XQuartz.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
  
-echo "Image: ubuntu_python_comp_sense"
+echo "Image: ubuntu_py_comp_sense"
 echo "Alias: PyCS"
 
 # run xhost and allow connections from your local machine:
@@ -8,7 +8,7 @@ echo "Alias: PyCS"
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
 
-docker run -it --name PyCS -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix ubuntu_python_comp_sense
+docker run -it --name PyCS -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix ubuntu_py_comp_sense
 
 xhost - $ip
 docker rm /PyCS
